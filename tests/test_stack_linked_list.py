2e60f745cdfd27d0811277b6b384e58aa643bfac
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-02-26 15:23:17 +0200

    test: add all tests  unit test and make it pass

---
+++ tests/test_stack_linked_list.py
@@ -0,0 +1,62 @@
+import pytest
+from stacks.stack_linked_list import Stack
+
+def test_push():
+    stack: Stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+
+    assert stack.elments.length == 3
+
+def test_pop():
+    stack: Stack = Stack()
+    empty_satck: Stack = Stack()
+
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+    poped_element = stack.pop()
+    assert poped_element == 30
+    assert stack.elments.length == 2
+
+    with pytest.raises(IndexError):
+        poped_element = empty_satck.pop()
+
+def test_peek():
+    stack: Stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+    top_element = stack.peek()
+
+    assert top_element == 30
+    assert stack.elments.length == 3
+
+def test_is_empty():
+    stack: Stack = Stack()
+    empty_stack: Stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+
+    assert stack.is_empty() is False
+    assert empty_stack.is_empty() is True
+
+def test_size():
+    stack: Stack = Stack()
+    empty_stack: Stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+    assert stack.size() == 3
+    assert empty_stack.size() == 0
+
+def test_clear():
+    stack: Stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+    assert stack.size() == 3
+    stack.clear()
+    assert stack.size() == 0
