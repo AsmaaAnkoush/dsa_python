6395d2acdbf93ca72d909e2eb284903633c564a9
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-02-26 14:51:15 +0200

    feat: implement pop function that remove and return the top element in stack

--- stacks/stack.py
+++ stacks/stack.py
@@ -9,7 +9,7 @@ class Stack:
         '''this function is constructor to the class Stack'''
         self.top = None
         self.length: int = 0
-        
+
     def push(self, data):
         '''this function add data in the top of stack: node point to old top and then updata top'''
         #Time Complexity is O(1)
@@ -18,3 +18,15 @@ class Stack:
         node.next = self.top
         self.top = node
         self.length += 1
+    
+    def pop (self):
+        '''this function remove and return the top element in stack'''
+        #Time Complexity is O(1)
+        #Space complexity is o(1)
+        if self.top == None:
+            raise IndexError ("The Stack is Empty")
+        else:
+            poped_data = self.top.value
+            self.top = self.top.next
+            self.length -= 1
+            return poped_data
