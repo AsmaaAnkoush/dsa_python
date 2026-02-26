bb3f7b6509ef7152e95f11d33d7bc6ac4119b705
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-02-26 12:02:10 +0200

    feat: implement push function that add data in the top of stack: node point to old top and then updata top

--- stacks/stack.py
+++ stacks/stack.py
@@ -9,3 +9,12 @@ class Stack:
         '''this function is constructor to the class Stack'''
         self.top = None
         self.length: int = 0
+        
+    def push(self, data):
+        '''this function add data in the top of stack: node point to old top and then updata top'''
+        #Time Complexity is O(1)
+        #Space complexity is o(1)
+        node: Node = Node (data)
+        node.next = self.top
+        self.top = node
+        self.length += 1
