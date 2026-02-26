54478f049e1d333172f63776dbe93d184b788fc6
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-02-26 11:16:48 +0200

    feat: implement enqueue function that add data to the end of queue

--- queues/queue.py
+++ queues/queue.py
@@ -10,3 +10,16 @@ class Queue:
         self.head = None
         self.tail = None
         self.length: int = 0
+
+    def enqueue(self, data):
+        '''this function add data to the end of queue'''
+        #Time Complexity O(1)
+        #Space complexity O(1)
+        node: Node = Node(data)
+        if self.length == 0:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
