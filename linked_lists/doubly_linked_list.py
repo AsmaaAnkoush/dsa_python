81e46ae5802b61bf118c03af6e5a56c70526e17a
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-02-26 10:49:34 +0200

    feat: implement insert_at_head function that add the value to the begning of double linked list

--- linked_lists/doubly_linked_list.py
+++ linked_lists/doubly_linked_list.py
@@ -11,3 +11,17 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
         self.length = 0
+    
+    def insert_at_head(self,data):
+        '''this function add the value to the begning of double linked list'''
+        #Time complexity = O(1)
+        #Space complexity = O(1)
+        node: DoublyNode = DoublyNode(data)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+        self.length += 1
