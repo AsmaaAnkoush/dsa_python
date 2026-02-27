452a2f9906ee0f7e6eedc908bb1cdf3695ae5b1a
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-02-27 10:32:14 +0200

    feat : implement insert function that add to the end of circular linked list

--- linked_lists/circular_linked_list.py
+++ linked_lists/circular_linked_list.py
@@ -8,3 +8,33 @@ class CircularLinkedList:
         self.head = None
         self.length = 0
     
+    def insert(self, data):
+        '''this function insert to the end of circular linkef list'''
+        #Time Complexity = O(n)
+        #Space Complexity = O(1)
+        node: CircularNode = CircularNode(data)
+
+        if self.head == None:
+            self.head = node
+            node.next = self.head
+            self.length += 1
+        else:
+            current: CircularNode = self.head
+            while current.next != self.head:
+                current = current.next
+            current.next = node
+            node.next = self.head
+            self.length += 1
+
+    def __str__(self):
+        '''this function for human-readable representation'''
+        if self.head == None:
+            return "Empty List"
+        string: str = "head -> "
+        current: CircularNode = self.head
+        string = string + f"{current.data} -> "
+        while current.next != self.head:
+                current = current.next
+                string = string + f"{current.data} -> "
+        string = string + "head" 
+        return string
