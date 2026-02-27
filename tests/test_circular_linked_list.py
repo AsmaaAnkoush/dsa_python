5bfccd2f8ab6c54c8cbd5093aa673d53f21396b2
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-02-27 14:15:00 +0200

    test : add test_length unit test and make it pass

--- tests/test_circular_linked_list.py
+++ tests/test_circular_linked_list.py
@@ -48,3 +48,10 @@ def test_get_at():
     assert cll.get_at(1) is 15
     assert cll.get_at(2) is 20
     assert cll.length is 3
+
+def test_length():
+    cll = CircularLinkedList()
+    cll.insert(10)
+    cll.insert(15)
+    cll.insert(20)
+    assert cll.size() is 3
